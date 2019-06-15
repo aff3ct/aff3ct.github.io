a2cd9d0d28334ad451a73834b45d90cd48d879a7
Adrien Cassagne <adrien.cassagne@inria.fr>
2019-06-15 16:27:36 +0200

    Add JS script to create the JSON Kaiserslautern database + add the database.

---
+++ js/gen_kldb.js
@@ -0,0 +1,45 @@
+var KLDB={};
+$(document).ready(function() {
+	$.ajaxSetup({
+		beforeSend: function(xhr) {
+			if (xhr.overrideMimeType) xhr.overrideMimeType("text/plain");
+		},
+		isLocal:false
+	});
+	let rootKL="https://www.uni-kl.de/";
+	let urlKL=rootKL+"/channel-codes/ml-simulation-results/";
+	$.ajax(urlKL,
+		{error:function(xhr,status,error) {
+			logger("**Error loading \"" + databaseURL + "\"\n"+status+" "+error);
+		},
+	}).done(function(html) {
+		let list = html.match(/href="\/fileadmin\/chaco\/public\/results_[^.]*\.txt/g);
+		// rm duplicates
+		let obj = {};
+		list.forEach(function(entry) {
+			let url = entry.match(/href="(\/fileadmin\/chaco\/public\/results_[^.]*\.txt)/)[1];
+			obj[url]="";
+		});
+		let urls = Object.keys(obj);
+		let cu=0;
+		urls.forEach(function(url) {
+			$.ajax(rootKL+url,
+				{error:function(xhr,status,error) {
+					logger("**Error loading \"" + databaseURL + "\"\n"+status+" "+error);
+				},
+			}).done(function(txt) {
+				let filename = url.match(/\/([^/.]*\.txt)/)[1];
+				let ref = text2jsonKaiserslautern(txt, filename, true);
+				KLDB[ref.hash.value.substring(0,7)]=ref;
+				cu++
+				if (cu==urls.length)
+				{
+					console.log(KLDB);
+					console.log(Object.keys(KLDB).length);
+					$("body pre").empty();
+					$("body pre").append(JSON.stringify(KLDB, null, 4));
+				}
+			});
+		});
+	});
+});
