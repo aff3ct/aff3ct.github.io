6fe5827d07289c86ba05652dc702edf7f388ce21
Adrien Cassagne <adrien.cassagne@inria.fr>
2019-12-15 19:20:02 +0100

    Use https for the CouchDB database.

--- js/comparator.js
+++ js/comparator.js
@@ -2,7 +2,7 @@ const GITLAB="https://gitlab.com/api/v4/projects/10354484/";
 const BRANCH="development";
 
 // connexion to the CouchDB server (this code do not connect to the CouchDB until the first usage)
-const ServerCDB = "http://panoramix.potionmagic.eu:5984";
+const ServerCDB = "https://panoramix.potionmagic.eu:5985";
 const NameCDB = "aff3ct_refs";
 var CDB = new PouchDB(ServerCDB+'/'+NameCDB);
 
